2235592f149f9d562e3b27d19b44c60d27bd77b9
Rodri Azuri <azuri.rodrigo@gmail.com>
2021-11-03 23:20:54 -0300

    Entrega Simulador Interactivo

--- js/ciclos.js
+++ js/ciclos.js
@@ -1,55 +1,26 @@
-let estilo = Number(prompt(`Ingrese el estilo que desea: 
-1 - Pop
-2 - Rock
-3 - Reggae
-4 - Electrónica
-5 - Rap
-6 - Trap
-7 - Música Clásica`));
-const mail = prompt("Ingrese su email");
+let edad = obtenerEdad();
+const mail = obtenerMail();
+let añoNacimiento = año(edad);
 
-
-while (estilo >= 1 || estilo <= 7) {
-
-    if (estilo < 1 || estilo > 7) {
-        alert("El número ingresado es incorrecto. Intentelo nuevamente");
-        estilo = Number(prompt(`Ingrese el estilo que desea: 
-        1 - Pop
-        2 - Rock
-        3 - Reggae
-        4 - Electrónica
-        5 - Rap
-        6 - Trap
-        7 - Música Clásica`));  
-    } 
-    
-    switch (estilo) {
-        case 1:
-            alert("Enviaremos música del estilo músical Pop al siguiente correo: " + mail);
-            break;
-        case 2:
-            alert("Enviaremos música del estilo músical Rock al siguiente correo: " + mail);
-            break;
-        case 3:
-            alert("Enviaremos música del estilo músical Reggae al siguiente correo: " + mail);
-            break;
-        case 4:
-            alert("Enviaremos música del estilo músical Electrónica al siguiente correo: " + mail);
-            break;
-        case 5:
-            alert("Enviaremos música del estilo músical Rap al siguiente correo: " + mail);
-            break;
-        case 6:
-            alert("Enviaremos música del estilo músical Trap al siguiente correo: " + mail);
-            break;
-        case 7:
-            alert("Enviaremos música del estilo Clásica al siguiente correo: " + mail);
-            break;
-    }
-     
-    break; 
+if (menorEdad(edad)) {
+    alert(`Usted es menor de Edad, no podemos enviarle música.`);
+} else {
+    alert(`Enviaremos música del año ${añoNacimiento} al siguiente correo: ${mail}`);
 }
 
 
+function año(edad) {
+    return 2021 - edad;
+}
 
+function menorEdad(edad) {
+    return edad < 18;
+}
 
+function obtenerEdad() {
+    return Number(prompt(`Ingrese su edad`));
+}
+
+function obtenerMail() {
+    return prompt("Ingrese su email");
+}
